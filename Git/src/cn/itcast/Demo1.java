5e0ea4722808cc58dd6bec656fefb41c0ce25806
12680205@qq.com <12680205@qq.com>
2019-06-12 16:21:52 +0800

    本地工程添加到git

---
+++ Git/src/cn/itcast/Demo1.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println(1111);
+        System.out.println(1111);
+        System.out.println(1111);
+        System.out.println(1111);
+        System.out.println(1111);
+    }
+}
